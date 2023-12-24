10a274dda97e1e9fd621ff8cc1f996bb541b2526
Amir Eydi <105715253+amirad1@users.noreply.github.com>
2023-12-24 08:24:18 +0330

    Update clean-up-jenkins-old-builds.sh

--- clean-up-jenkins-old-builds.sh
+++ clean-up-jenkins-old-builds.sh
@@ -5,3 +5,6 @@ JENKINS_ADDRESS=/var/lib/jenkins/jobs
 
 #removes all the builds except the latest one and two other files called legacyIds permalinks
 find $JENKINS_ADDRESS -name "build" -type d | while read dir; do (cd "$dir" && ls -d */ | sort -n | head -n -1 | xargs rm -rf); done
+
+#if you want to skip dev and master  branches
+find /var/lib/jenkins/jobs/ -name "builds" -type d | grep -vE '/(master|dev)/builds$' | while read dir; do (cd "$dir" && ls -d */ | sort -n | head -n -1 | xargs rm -rf); done
