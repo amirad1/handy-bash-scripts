b30011e47182aa5badb28069b555eafba8940954
Amir Eydi <105715253+amirad1@users.noreply.github.com>
2024-02-05 10:24:16 +0330

    Update show-docker-container-ip-addresses.sh

--- show-docker-container-ip-addresses.sh
+++ show-docker-container-ip-addresses.sh
@@ -2,6 +2,16 @@
 
 #!/bin/sh
 
-contid=($(docker ps -q))
+var=($(docker ps -q))
 
-for i in ${contid[@]}; do docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $i ;done
+for n in ${var[@]};
+
+do
+
+ip_address=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$n")
+container_name=$(docker inspect -f '{{.Name}}' "$n" | sed 's/\///g')
+
+echo "$container_name : $ip_address";
+
+
+done
