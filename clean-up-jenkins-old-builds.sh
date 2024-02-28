ea0c6f890ca5145d5a67918b3f76d9c24e1a4458
Amir Eydi <105715253+amirad1@users.noreply.github.com>
2024-02-28 16:04:45 +0330

    Update clean-up-jenkins-old-builds.sh

--- clean-up-jenkins-old-builds.sh
+++ clean-up-jenkins-old-builds.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-#write jenkins home directory address
-JENKINS_ADDRESS='/var/lib/jenkins/jobs'
+#get jenkins home directory address
+read JENKINS_ADDRESS
+
+#default jenkins address
+#JENKINS_ADDRESS='/var/lib/jenkins/jobs'
 
 #clean up old jenkins builds except 5 latest builds and dev and master branch
 find $JENKINS_ADDRESS -name "builds" -type d | grep -vE '/(master|dev)/builds$' | while read dir; do (cd "$dir" && ls -d */ | sort -n | head -n -5 | xargs rm -rf); done
