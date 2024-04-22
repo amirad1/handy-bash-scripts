776635a15b6b5e03d75aa6f39d985e90af246fb9
Amir Eydi <105715253+amirad1@users.noreply.github.com>
2024-04-22 17:54:39 +0330

    Update kubernetes-pod-ip-finder.sh

--- kubernetes-pod-ip-finder.sh
+++ kubernetes-pod-ip-finder.sh
@@ -1,26 +1,6 @@
 #!/bin/sh
 #find-k8s-pods-lists
 
-#!/bin/bash
-
-# Define the ASCII art
-ASCII_ART="
-    .--.
-   |o_o |
-   |:_/ |
-  //   \ \\
- (|     | )
-/'\\_   _/`\\
-\\___)=(___/
-GitHub: github.com/amirad1
-"
-
-# Display the ASCII art
-echo "$ASCII_ART"
-
-# Your script continues here...
-
-
 echo "                  "
 
 kubectl get ns | awk 'NR > 1 {print $1}'
